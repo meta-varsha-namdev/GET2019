4c33000486a7b9dfb48c8ba85499e244978238c4
meta-varsha-namdev <46709985+meta-varsha-namdev@users.noreply.github.com>
2019-02-01 17:03:09 +0530

    Update Shape.java

--- Session-9/Assignment-shapes/Shape.java
+++ Session-9/Assignment-shapes/Shape.java
@@ -1,14 +1,13 @@
 public interface Shape 
 {
-	
- public double getArea();
- public double getPerimeter();
- public Point getOrigin();
- public boolean isPointEnclosed(Point point);
- public String getType();
- // creating enum function for the shapes which can be created
- public enum ShapeType
- {
-  rectangle,square,circle,triangle;
- }
+	public double getArea();
+	public double getPerimeter();
+	public Point getOrigin();
+	public boolean isPointEnclosed(Point point);
+	public String getType();
+	// creating enum function for the shapes which can be created
+	public enum ShapeType
+	{
+		rectangle,square,circle,triangle;
+	}
 }
