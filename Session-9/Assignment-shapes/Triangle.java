34bd60a627e37a53439ef9e3dd63e36dbf0b26a5
meta-varsha-namdev <46709985+meta-varsha-namdev@users.noreply.github.com>
2019-02-01 17:07:07 +0530

    Update Triangle.java

--- Session-9/Assignment-shapes/Triangle.java
+++ Session-9/Assignment-shapes/Triangle.java
@@ -5,48 +5,49 @@ public class Triangle implements Shape
 	 private final String shape="triangle";
 	 public Triangle(Point point,double length_1, double length_2, double length_3)// passing the arguments to the constructor
 	 {
-	     this.point = point;
-	     this.length_1=length_1;
-	     this.length_2=length_2;
-	     this.length_3=length_3;
+	 	this.point = point;
+		this.length_1=length_1;
+		this.length_2=length_2;
+		this.length_3=length_3;
 	 }
-	// getting the length of the three sides of the triangle
+	 // getting the length of the three sides of the triangle
 	 public double getlength_1()
 	 {
-	  return this.length_1;
+	  	return this.length_1;
 	 }
 	 public double getlength_2()
 	 {
-	  return this.length_2;
+	  	return this.length_2;
 	 }
 	 public double getlength_3()
 	 {
-	  return this.length_3;
+	 	return this.length_3;
 	 }
 	 
 	 public String getType()// getting the type of the shape
 	 {
-	  return this.shape;
+	  	return this.shape;
 	 }
-		@Override
+	 
+	 @Override
 	 public double getArea()//calculating the area of the triangle
 	 {
-      double semiperimeter=(length_1+length_2+length_3)/2;//calculating the semi-perimeter
-	  double area =Math.pow(((semiperimeter)*(semiperimeter-length_1)*(semiperimeter-length_2)*(semiperimeter-length_3)),0.5);//Heron's formula
-	  return area;
+	 	double semiperimeter=(length_1+length_2+length_3)/2;//calculating the semi-perimeter
+	  	double area =Math.pow(((semiperimeter)*(semiperimeter-length_1)*(semiperimeter-length_2)*(semiperimeter-length_3)),0.5);//Heron's formula
+	  	return area;
 	 }
-		@Override
+	 @Override
 	 public double getPerimeter()// calculating the perimeter of the triangle
 	 {
-	  double perimeter = (length_1+length_2+length_3);
-	  return perimeter;
+	  	double perimeter = (length_1+length_2+length_3);
+	  	return perimeter;
 	 }
-		@Override
+	 @Override
 	 public Point getOrigin()// getting the origin of the triangle
 	 {
-	  return this.point;
+	  	return this.point;
 	 }
-		@Override
+	 @Override
 	 public boolean isPointEnclosed(Point point)//checking if the given point is enclosed by the triangle or not
 	 {
 	   
